1deb1d920a46ec74e3350c70709cd497b1b0df9a
yugoru <yulia.gorshkova@soft-machine.ru>
2021-04-05 10:17:34 +0300

    Первый пуш

---
+++ src/test/java/tests/AllTestsRun.java
@@ -0,0 +1,44 @@
+package tests;
+
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Запуск всех тестов сразу")
+public class AllTestsRun {
+
+    private final StepTest steps = new StepTest();
+    private final LambdaTest lambda = new LambdaTest();
+    private final SelenideTest selenide = new SelenideTest();
+
+    private final static String REPO = "allure-framework/allure2";
+    private final static String PAGE_NAME = "Issues";
+    private final static String SEARCH_FOR = "#1198";
+
+    @Test
+    @DisplayName("StepTest")
+    @Owner("yugoru")
+    void searchRepoStep() {
+        steps.openMainPage();
+        steps.searchRepo(REPO);
+        steps.clickRepo(REPO);
+        steps.goToPage(PAGE_NAME);
+        steps.searchFor(SEARCH_FOR);
+    }
+
+    @Test
+    @DisplayName("LambdaTest")
+    @Owner("yugoru")
+    void searchRepoLambda() {
+        lambda.searchIssue(REPO, PAGE_NAME, SEARCH_FOR);
+    }
+
+    @Test
+    @DisplayName("SelenideTest")
+    @Owner("yugoru")
+    void searchRepoSelenide() {
+        selenide.searchIssue(REPO, PAGE_NAME, SEARCH_FOR);
+    }
+
+
+}
---
+++ src/test/java/tests/StepTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class StepTest {
+
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем нужный репозиторий в поле для ввода")
+    public void searchRepo(String repo) {
+        $(".header-search-input").setValue(repo).pressEnter();
+
+    }
+
+    @Step("Переходим в репозиторий")
+    public void clickRepo(String repo) {
+        $(By.linkText(repo)).click();
+    }
+
+    @Step("Переходим на указанную страницу")
+    public void goToPage(String pageName) {
+        $(withText(pageName)).click();
+
+    }
+
+    @Step("Открываем главную страницу")
+    public void searchFor(String searchFor) {
+        $(withText(searchFor)).shouldBe(Condition.visible);
+
+    }
+
+}
+
+
+
